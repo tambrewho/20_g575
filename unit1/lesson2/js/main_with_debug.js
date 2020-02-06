c3dd7ca263241e3ed992e05419989e988465b7c0
Tambre Hu <thu.1215@gmail.com>
2020-02-05 18:41:06 -0600

    Activity 3
    Activity 3: Cities Table and Debugging 1

--- unit1/lesson2/js/main_with_debug.js
+++ unit1/lesson2/js/main_with_debug.js
@@ -5,9 +5,10 @@ function initialize(){
 
 //function to create a table with cities and their populations
 function cities(){
+
 	//define two arrays for cities and population
 	var cityPop = [
-		{ 
+		{
 			city: 'Madison',
 			population: 233209
 		},
@@ -30,10 +31,10 @@ function cities(){
 
 	//append a header row to the table
 	$("table").append("<tr>");
-	
+
 	//add the "City" and "Population" columns to the header row
 	$("tr").append("<th>City</th><th>Population</th>");
-	
+
 	//loop to add a new row for each city
     for (var i = 0; i < cityPop.length; i++){
         //assign longer html strings to a variable
@@ -46,61 +47,70 @@ function cities(){
     addEvents();
 };
 
-function addColumns(cityPop){
-    
-    $('tr').each(function(i){
+// function to add columns for the city size
+function addColumns(cityPop) {
 
-    	if (i == 0){
+	$('tr').each(function(i) {
 
-    		$(this).apend('<th>City Size</th>');
-    	} else {
+		if (i == 0){
+    	$(this).append('<th>City Size</th>');
+    } else {
 
-    		var citySize;
+  		var citySize;
 
-    		if (cityPop[i-1].population < 100000){
-    			citySize = 'Small';
+			// assigns each element a city size (Small, Medium, Large)
+  		if (cityPop[i-1].population < 100000){
+  			citySize = 'Small';
 
-    		} else if (cityPop[i-1].population < 500000){
-    			citysize = 'Medium';
+  		} else if (cityPop[i-1].population < 500000){
+  			citySize = 'Medium';
 
-    		} else {
-    			citySize = 'Large';
-    		};
+  		} else {
+  			citySize = 'Large';
+  		};
 
-    		$this.append('<td' + citySize + '</td>');
-    	};
-    });
+			// adds each respective city's size
+  		$(this).append('<td>' + citySize + '</td>');
+  	};
+
+  });
+
 };
 
+// function for adding user interaction events
 function addEvents(){
 
-	$('#table').mouseover(function(){
-		
+	//event handler for whenever the user mouses over the table
+	$("table").mouseover(function() {
+
 		var color = "rgb(";
 
 		for (var i=0; i<3; i++){
-
 			var random = Math.round(Math.random() * 255);
 
-			color += "random";
+			color += random;
 
 			if (i<2){
 				color += ",";
-			
+
 			} else {
 				color += ")";
+			};
+
+			// will change the color of the table each time the event is triggered
+			$(this).css('color', color);
 		};
 
-		$(this).css('color', color);
 	});
 
+	// function for the action of clicking on the table
 	function clickme(){
-
 		alert('Hey, you clicked me!');
 	};
 
+	// add event listener for clickme to table
 	$('table').on('click', clickme);
-};
+}
 
 //call the initialize function when the document has loaded
 $(document).ready(initialize);
